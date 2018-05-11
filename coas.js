2151f592f6b5836ab3b5ab218d7f3628e24f3fdc
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-11 14:14:42 -0700

    Update coas.js

--- coas.js
+++ coas.js
@@ -1,7 +1,6 @@
 var val;
-var out = document.getElementById('out');
 function addone() {
-  setInterval(function() { out.innerText = val }, 10);
+  setInterval(function() { document.getElementById('out').innerText = val }, 10);
   val = 0;
   val++;
 }
