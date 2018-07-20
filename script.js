3a7d88c4bc82a5bd8929fd0f71931a778e59170e
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-20 15:35:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ if(localStorage.dev === '$_dev') {
 }
 
 setInterval(function() {
-	if(document.getElementById('home').style.display = 'block') {
+	if(document.getElementById('home').style.display === 'block') {
 		document.title = localStorage.name+'\'s profile'
 	}
 }, 100)
