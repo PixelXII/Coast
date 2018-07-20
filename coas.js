f260692805d4f7fc1170f876581fff2bcb0633f8
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-20 12:30:49 -0700

    Delete coas.js

--- coas.js
+++
@@ -1,9 +0,0 @@
-var val = 0;
-function addone() {
-  setInterval(function() { document.getElementById('out').innerText = val }, 20);
-  val++;
-  if(val >= 100) {
-    val = "You reached 100! Good job!";
-    document.getElementById('addButton').removeAttribute('onclick');
-  }
-}
