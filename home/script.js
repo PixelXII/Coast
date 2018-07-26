7ddac252329a68532d9905e65c172caefbddb0df
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-25 20:55:03 -0700

    Update script.js

--- home/script.js
+++ home/script.js
@@ -1,1 +1,6 @@
+var profile = document.getElementById('profile')
+var profileImg = document.getElementById('profim')
+var nameOut = document.getElementById('name')
 
+nameOut.innerText = localStorage.name
+profileImg.src = localStorage.profile + '.png'
