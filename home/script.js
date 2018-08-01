e30795114a22b77dbcf78e91d3caa6186bbc957e
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-31 21:29:14 -0700

    Update script.js

--- home/script.js
+++ home/script.js
@@ -1,6 +1,48 @@
 var profile = document.getElementById('profile')
 var profileImg = document.getElementById('profim')
 var nameOut = document.getElementById('name')
+localStorage.home = true
 
 nameOut.innerText = localStorage.name
 profileImg.src = '../images/' + localStorage.profile + '.png'
+
+document.getElementById('desc').addEventListener('click', function() {
+	configDesc()
+	document.getElementById('link').style.display = 'none'
+});
+
+document.getElementById('password').addEventListener('keypress', function(e) {
+	if(e.keyCode === 13) {
+		setPassword()
+	}
+});
+
+function setProfile(img) {
+	document.getElementById('profileimg').src = 'images/'+img+'.png'
+	localStorage.profile = img
+}
+
+function changeImage() {
+	const pig = document.getElementById('pimg')
+	if(pig.style.display === 'none') {
+		pig.style.display = 'block'
+	} else {
+		pig.style.display = 'none'
+	}
+	if(document.getElementById('password-setup').style.display === 'block') {
+		document.getElementById('password-setup').style.display = 'none'
+	}
+	if(document.getElementById('colors').style.display === 'block') {
+		document.getElementById('colors').style.display = 'none'
+	}
+}
+
+document.getElementById('descIn').addEventListener('keypress', function(e) {
+	const cf = document.getElementById('confd')
+	if(e.keyCode === 13) {
+		submitdesc()
+		document.getElementById('link').style.display = 'block'
+		cf.style.display = "none"
+		cf.value = ""
+	} 
+});
