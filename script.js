5752c3af4d3cda45927bf4445e47304b47a0815d
altD <35429528+PixelXII@users.noreply.github.com>
2018-07-31 20:58:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,18 +33,19 @@ document.getElementById('descIn').addEventListener('keypress', function(e) {
 	const cf = document.getElementById('confd')
 	if(e.keyCode === 13) {
 		submitdesc()
+		document.getElementById('link').style.display = 'block'
 		cf.style.display = "none"
 		cf.value = ""
 	} 
 });
 
-document.getElementById('descIn').addEventListener('keypress', function(e) {
+/* document.getElementById('descIn').addEventListener('keypress', function(e) {
 	const cf = document.getElementById('confd')
 	if(e.keyCode === 27) {
 		cf.style.display = 'none'
 		cf.value = ''
 	}
-});
+}); */
 
 function setProfile(img) {
 	document.getElementById('profileimg').src = 'images/'+img+'.png'
@@ -68,6 +69,7 @@ function changeImage() {
 
 document.getElementById('desc').addEventListener('click', function() {
 	configDesc()
+	document.getElementById('link').style.display = 'none'
 });
 
 document.getElementById('password').addEventListener('keypress', function(e) {
