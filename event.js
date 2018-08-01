08520e275f97687fe76260d9ff67f6642579069f
altD <35429528+PixelXII@users.noreply.github.com>
2018-08-01 16:20:32 -0700

    Update event.js

--- event.js
+++ event.js
@@ -1,1 +1,34 @@
+namee.addEventListener('keypress', function(e) {
+	if(e.keyCode === 13) {
+		processName()
+	}
+});
 
+document.getElementById('descIn').addEventListener('keypress', function(e) {
+	const cf = document.getElementById('confd')
+	if(e.keyCode === 13) {
+		submitdesc()
+		document.getElementById('link').style.display = 'block'
+		cf.style.display = "none"
+		cf.value = ""
+	} 
+});
+
+/* document.getElementById('descIn').addEventListener('keypress', function(e) {
+	const cf = document.getElementById('confd')
+	if(e.keyCode === 27) {
+		cf.style.display = 'none'
+		cf.value = ''
+	}
+}); */
+
+document.getElementById('desc').addEventListener('click', function() {
+	configDesc()
+	document.getElementById('link').style.display = 'none'
+});
+
+document.getElementById('password').addEventListener('keypress', function(e) {
+	if(e.keyCode === 13) {
+		setPassword()
+	}
+});
