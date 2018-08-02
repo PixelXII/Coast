98b5506ed893e608c496036a00aebed5da58b66f
altD <35429528+PixelXII@users.noreply.github.com>
2018-08-01 17:04:01 -0700

    Update classes.js

--- home/classes.js
+++ home/classes.js
@@ -1,1 +1,15 @@
+function Post(title, content) {
+  this.title = title
+  this.content = content
+  var post = document.createElement('div')
+  document.getElementById('content').appendChild(post)
+  post.innerHTML = "<h2 class='title'>"+title+"</h2> <br> <p id='content'>"+content+"</p>"
+}
 
+Post.prototype.edit = function() {
+  // PATIENCE, IM WORKING ON IT
+  
+  // actually im not
+  
+  // but i leave it here to remind me that i *want* to add it
+}
