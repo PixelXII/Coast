5fa8439b1f54da15110023f7169544a61a64534a
altD <35429528+PixelXII@users.noreply.github.com>
2018-08-01 20:58:54 -0700

    Update classes.js

--- home/classes.js
+++ home/classes.js
@@ -30,5 +30,6 @@ document.getElementById('pcon').addEventListener('keypress', function(e) {
    		var np = new Post(title, content)
     		posts.push(np)
 		document.getElementById('postD').style.display = 'none'
+		document.getElementById('content').style.display = 'block'
   	}
 });
