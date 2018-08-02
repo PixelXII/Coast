c292926082be3ae0a611460d4255e37777974779
altD <35429528+PixelXII@users.noreply.github.com>
2018-08-01 17:58:03 -0700

    override from last week

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const namee = document.getElementById('name')
 var name, description;
 var deeppurple = '#673AB7', indigo = '#3F51B5', teal = '#009688', bluegrey = '#607D8B';
 var na;
-throw new Error()
-localStorage.desc = "No description provided"
-localStorage.profile = 9
 if(document.title.toString().includes('undefined')) {
 	localStorage.clear()
 	location.reload()
 }
-
-if(namee.innerText === 'undefined') {
-	document.getElementById('first-screen').style.display = 'block'
+if(localStorage.dev === '$_dev') {
+	dev()
+	if(localStorage.profile != undefined) {
+		localStorage.profile = 'dev'
+		document.getElementById('profileimg').src = 'images/dev.png'
+	}
 }
 
 setInterval(function() {
@@ -22,6 +22,29 @@ setInterval(function() {
 if(namee.innerText = "$_dev") {
 	namee.innerText = 'kia'
 }
+
+namee.addEventListener('keypress', function(e) {
+	if(e.keyCode === 13) {
+		processName()
+	}
+});
+
+document.getElementById('descIn').addEventListener('keypress', function(e) {
+	const cf = document.getElementById('confd')
+	if(e.keyCode === 13) {
+		submitdesc()
+		cf.style.display = "none"
+		cf.value = ""
+	} 
+});
+
+document.getElementById('descIn').addEventListener('keypress', function(e) {
+	const cf = document.getElementById('confd')
+	if(e.keyCode === 27) {
+		cf.style.display = 'none'
+		cf.value = ''
+	}
+});
 
 function setProfile(img) {
 	document.getElementById('profileimg').src = 'images/'+img+'.png'
@@ -43,10 +66,15 @@ function changeImage() {
 	}
 }
 
-function reset() {
-	localStorage.clear()
-	location.reload()
-}
+document.getElementById('desc').addEventListener('click', function() {
+	configDesc()
+});
+
+document.getElementById('password').addEventListener('keypress', function(e) {
+	if(e.keyCode === 13) {
+		setPassword()
+	}
+});
 
 function colors() {
 	var col = document.getElementById('colors')
@@ -79,6 +107,18 @@ function displaySettings() {
 	} else {
 		sett.style.display = 'none'
 	}
+}
+
+function dev() {
+	document.getElementById('desc').innerHTML = "I am your creator."
+	localStorage.desc = "I am your creator."
+	localStorage.name = "kia"
+	localStorage.dev = '$_dev'
+	document.getElementById('profileimg').src = "images/dev.png"
+	document.getElementById('passet').style.display = 'none'
+	document.getElementById('pcontainer').style.display = 'none'
+	localStorage.pass = 'QW5pbWUgcm9ja3M='
+	removeEventListener('click', document.getElementById('desc'))
 }
 
 function processName() {
@@ -191,7 +231,7 @@ function enterPass() {
 
 function store() {
 	if(localStorage.length != 0) {
-		if(localStorage.pass != undefined) {
+		if(localStorage.pass != null) {
 			document.getElementById('home').style.display = 'none'
 			document.getElementById('first-screen').style.display = 'none'
 			document.getElementById('pass-screen').style.display = 'block'
